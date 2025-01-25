e58b56636ce6ee41e62cfcc3f1a33f0796a7b345
jessica <jepersio@hotmail.com>
2025-01-24 23:40:13 -0300

    Refactor Home component to improve Pokémon fetching and filtering logic; add shiny toggle functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,86 +1,102 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import PokemonCard from '../components/PokemonCard'
-import { Container } from '@mui/system'
-import { Box, Grid, Switch, Tooltip } from '@mui/material'
-import axios from 'axios'
-import { Skeletons } from '../components/Skeletons'
-import '../App.css';
-import { ThemeProvider } from '@emotion/react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard";
+import { Container } from "@mui/system";
+import { Box, Grid } from "@mui/material";
+import axios from "axios";
+import { Skeletons } from "../components/Skeletons";
+import "../App.css";
+import { ThemeProvider } from "@emotion/react";
 import { useNavigate } from "react-router-dom";
-import { theme } from '../components/Theme'
+import { theme } from "../components/Theme";
 
 export const Home = ({ setPokemonData }) => {
-    
-    const [pokemons, setPokemons] = useState([])
-    const navigate = useNavigate();
-    useEffect(() => {
-        getPokemons()
-    }, [])
-    
-    const getPokemons = () => {
-        var endpoints = []
-        const languageCode = 'pt';
-        for ( var i = 1; i < 120; i++){
-            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/?lang=${languageCode}`)
-        }
-        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res)).catch((err) => console.log(err)) 
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+  const [shinyChecked, setShinyChecked] = useState(false);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  const fetchPokemons = async () => {
+    try {
+      const endpoints = [];
+      const languageCode = "pt";
+      for (let i = 1; i < 120; i++) {
+        endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/?lang=${languageCode}`);
+      }
+      const responses = await axios.all(endpoints.map((endpoint) => axios.get(endpoint)));
+      setAllPokemons(responses);
+      setPokemons(responses);
+    } catch (error) {
+      console.error("Erro ao buscar Pokémons:", error);
     }
+  };
 
-    const pokemonFilter= (name) => {
-        var filteredPokemons = []
-        if(name===""){
-            getPokemons()
-        }
-        for (var i in pokemons) {
-            if(pokemons[i].data.name.includes(name)) {
-                filteredPokemons.push(pokemons[i])
-            }
-        }
-        setPokemons(filteredPokemons)
+  const pokemonFilter = (name) => {
+    if (!name) {
+      setPokemons(allPokemons);
+    } else {
+      const filtered = allPokemons.filter((pokemon) =>
+        pokemon.data.name.toLowerCase().includes(name.toLowerCase())
+      );
+      setPokemons(filtered);
     }
-    const pokemonPickHandler = (pokemonData) => {
-        setPokemonData(pokemonData);
-        navigate("/profile");
-    };
-    
-    const [checked, setChecked] = useState(false);
-    const handleChange = (event) => {
-        setChecked(event.target.checked);
-    };    
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-            <div className='app'> 
-                <Navbar pokemonFilter={pokemonFilter} /> 
-                <Container maxWidth="false">
-                <Tooltip title='Shiny?'> 
-                    <Switch
-                    checked={checked}
-                    onChange={handleChange}
-                    inputProps={{ 'aria-label': 'controlled' }}
-                    color="secondary"
-                    />
-                </Tooltip>
-                
-                    <Grid container spacing={2}>
-                        {pokemons.length === 0 ? ( <Skeletons/> 
-                        ) : ( 
-                            pokemons.map((pokemon, key) => (
-                            <Grid item xs={12} sm={6} md={4} lg={4} key={key}>
-                                <Box onClick={() => pokemonPickHandler(pokemon.data)}>
-                                    <PokemonCard name={pokemon.data.name} image={checked ? pokemon.data.sprites.front_shiny : pokemon.data.sprites.front_default} types={pokemon.data.types} />
-                                </Box>
-                            </Grid>    
-                            ))
-                        )}
-                        
-                        
+  const pokemonPickHandler = (pokemonData) => {
+    setPokemonData(pokemonData);
+    navigate("/profile");
+  };
+
+  const handleShinyToggle = (checked) => {
+    setShinyChecked(checked); // Atualiza o estado do botão shiny
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <Navbar 
+          pokemonFilter={pokemonFilter} 
+          hideSearch={false} 
+          onShinyToggle={handleShinyToggle} 
+          shinyChecked={shinyChecked} 
+        />
+        <Container maxWidth="false" sx={{ marginTop: "2em" }}>
+            <Grid 
+                container 
+                spacing={2} 
+                justifyContent="center" // Centraliza os itens no container
+            >
+                {pokemons.length === 0 ? (
+                <Skeletons />
+                ) : (
+                pokemons.map((pokemon, index) => (
+                    <Grid 
+                    item 
+                    xs={12} sm={4} md={4} lg={3} // Exibe mais cards por linha
+                    key={index}
+                    >
+                    <Box onClick={() => pokemonPickHandler(pokemon.data)}>
+                        <PokemonCard
+                        name={pokemon.data.name}
+                        image={
+                            shinyChecked
+                            ? pokemon.data.sprites.front_shiny
+                            : pokemon.data.sprites.front_default
+                        }
+                        types={pokemon.data.types}
+                        />
+                    </Box>
                     </Grid>
-                </Container>
+                ))
+                )}
+            </Grid>
+        </Container>
 
-            </div>
-        </ThemeProvider>
-    )
-}
+      </div>
+    </ThemeProvider>
+  );
+};
